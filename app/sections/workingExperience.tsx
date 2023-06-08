f9a7931812265a0bbf53a5cd26890a29ef6de736
Kamin <ta-zaza2012@hotmail.com>
2023-06-08 21:42:22 +0700

    first commit

---
+++ app/sections/workingExperience.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+
+export default function WorkingExp() {
+    return (
+        <section id="workingExperience">
+        <h2>
+          <b>Working Experience</b>
+        </h2>
+        <Image src="/logo-w.png"alt='NextHopLogo'width={180}height={77}/>
+        <h2>Nov 2022 - Mar 2023  ~ 4 months</h2>
+        <h2>Role</h2>
+        <p>Trainee Flutter Developer</p>
+        <h2>Responsibilities</h2>
+        <ul>
+            <li>
+            Learn and build android mobile application as assigned with Flutter framework
+            </li>
+            <li>
+            Learn and build  a demo TodoList WebApp with ReactJS , TailWindUI/ MUI.
+            </li>
+        </ul>
+      </section>
+    )
+}
